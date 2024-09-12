532f38b89d35d47d9fc566a042b4f72f1649a624
sUmi7ro <simonto.no@outlook.com>
2024-09-12 23:37:18 +0200

    Start

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
+        int number = 9;
+        moveMany(number);
+        java.lang.String start = "A";
+        String help = "B";
+        String end = "C";
+    }
+
+    public static void moveMany(int n) {
+        //Base statement
+        if(n == 1){
+            moveOne();
+        }
+
+
+        moveMany(n-1); // Move many from start to help.
+        moveOne(); //Move one from bottom to the end.
+        moveMany(n-1); // Move many from help to end.
+    }
+
+    public static void moveOne(){
 
     }
 }
