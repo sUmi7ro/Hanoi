ffb38c0b6cb1df70448b17ad8ad3550a53371aed
sUmi7ro <simonto.no@outlook.com>
2024-09-13 01:28:45 +0200

    This doesnt work

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,26 @@
+import java.lang.*;
+
 public class Main {
+    public static String start = "A", help = "B", end = "C";
     public static void main(String[] args) {
         int number = 9;
-        moveMany(number);
-        java.lang.String start = "A";
-        String help = "B";
-        String end = "C";
+        System.out.println(start+" "+help+" "+end);
+        moveMany(number, start, help, end);
     }
 
-    public static void moveMany(int n) {
+    public static void moveMany(int n, String start, String help, String end) {
         //Base statement
         if(n == 1){
-            moveOne();
+            moveOne(start, end);
         }
-
-
-        moveMany(n-1); // Move many from start to help.
-        moveOne(); //Move one from bottom to the end.
-        moveMany(n-1); // Move many from help to end.
+        else{
+            moveMany(n-1, start, help, end); // Move many from start to help.
+            moveOne(start, end); //Move one from bottom to the end.
+            moveMany(n-1, end, help, start); // Move many from help to end.
+        }
     }
 
-    public static void moveOne(){
-
+    public static void moveOne(String st, String en){
+        System.out.println(en+" "+help+" "+st);
     }
 }
